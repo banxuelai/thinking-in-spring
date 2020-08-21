4a991fc2bf3b5bbcb36801afdc00387ce2d1ec20
banxuelai001 <banxuelai001@ke.com>
2020-08-21 10:46:36 +0800

    ioc-container-overview

---
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/dependency/lookup/DependencyLookUpDemo.java
@@ -0,0 +1,21 @@
+package org.geekbang.thinking.in.springioc.overview.dependency.lookup;
+
+import org.geekbang.thinking.in.springioc.overview.dependency.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author banxuelai001
+ * @date 2020/8/21
+ */
+public class DependencyLookUpDemo {
+
+    public static void main(String[] args) {
+
+        // 配置 xml配置文件
+        // 启动 Spring 应用上下文
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-lookup-context.xml");
+        User user = (User)beanFactory.getBean("user");
+        System.out.print(user);
+    }
+}
