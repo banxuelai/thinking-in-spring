65fb9a54fd5b0c8d61772b02223d8ab7b47e5501
banxuelai001 <banxuelai001@ke.com>
2022-02-09 16:19:51 +0800

    ioc容器

---
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/container/ApplicationContextAsIoCContainerDemo.java
@@ -0,0 +1,41 @@
+package org.geekbang.thinking.in.springioc.overview.container;
+
+import org.geekbang.thinking.in.springioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import java.util.Map;
+
+/** IoC容器 示例
+ * @author bxl
+ * @date 2022/1/7
+ */
+public class ApplicationContextAsIoCContainerDemo {
+
+    public static void main(String[] args) {
+        // 创建beanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+
+        // xml配置文件 classpath
+        String location  = "classpath:/META-INF/dependency-lookup-context.xml";
+
+        // 加载配置
+        int beanDefinitionsCount = reader.loadBeanDefinitions(location);
+
+        System.out.println("Bean定义加载的数量：" + beanDefinitionsCount);
+
+        // 依赖查找集合对象
+        lookupCollectionsByType(beanFactory);
+    }
+
+    private static void lookupCollectionsByType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找到的所有User 集合对象：" + users);
+        }
+    }
+}
--- think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/dependency/lookup/DependencyLookUpDemo.java
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/dependency/lookup/DependencyLookUpDemo.java
@@ -2,7 +2,10 @@ package org.geekbang.thinking.in.springioc.overview.dependency.lookup;
 
 import org.geekbang.thinking.in.springioc.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 /**
  * @author banxuelai001
@@ -17,5 +20,18 @@ public class DependencyLookUpDemo {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-lookup-context.xml");
         User user = (User)beanFactory.getBean("user");
         System.out.print(user);
+    }
+
+    private static void lookupCollectionsByType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找到的所有User 集合对象：" + users);
+        }
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("实时查找：" + user);
     }
 }
