40648a7dd7fcceeb1fa93a29ec42410e1878c6df
banxuelai001 <banxuelai001@ke.com>
2022-02-11 16:58:44 +0800

    spring ioc

---
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/container/AnnotationApplicationContextAsIoCContainerDemo.java
@@ -0,0 +1,51 @@
+package org.geekbang.thinking.in.springioc.overview.container;
+
+import org.geekbang.thinking.in.springioc.overview.domain.User;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * 注解能力 （@link ApplicationContext） 作为IoC容器示例
+ * @author bxl
+ * @date 2022/2/10
+ */
+@Configuration
+public class AnnotationApplicationContextAsIoCContainerDemo {
+
+    public static void main(String[] args) {
+
+        // 创建beanFactory容器
+        AnnotationConfigApplicationContext applicationContext  = new AnnotationConfigApplicationContext();
+        // 将当前类AnnotationApplicationContextAsIoCContainerDemo 作为配置类
+        applicationContext.register(AnnotationApplicationContextAsIoCContainerDemo.class);
+        // 启动应用上下文
+        applicationContext.refresh();
+
+        lookupCollectionByType(applicationContext);
+
+        // 关闭应用上下文
+        applicationContext.close();
+    }
+
+    // 通过java注解的方式定义一个bean
+//    @Bean
+//    public User user{
+//        User user = new User();
+//        user.setId(1L);
+//        user.setName("小马哥");
+//        return user;
+//    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory) beanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找到的所有的 User 集合对象：" + users);
+        }
+    }
+}
