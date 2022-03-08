3fae98c4890c020bf0e5fe1ed64dbadde25db7d4
banxuelai001 <banxuelai001@ke.com>
2022-03-08 11:01:03 +0800

    object_provider

--- think-in-spring/dependency-lookup/src/main/java/org/geekbang/thinking/in/dependency/lookup/ObjectProviderDemo.java
+++ think-in-spring/dependency-lookup/src/main/java/org/geekbang/thinking/in/dependency/lookup/ObjectProviderDemo.java
@@ -1,9 +1,11 @@
 package org.geekbang.thinking.in.dependency.lookup;
 
+import org.geekbang.thinking.in.springioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
 
 /**
  * 通过 {@link org.springframework.beans.factory.ObjectProvider} 实现依赖查找
@@ -25,13 +27,36 @@ public class ObjectProviderDemo { // @Configuration 是非必须的注解
         // 依赖查找
         lookupByObjectProvider(applicationContext);
 
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
+
         // 关闭应用上下文
         applicationContext.close();
     }
 
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> objectProvider = applicationContext.getBeanProvider(User.class);
+
+        User user = objectProvider.getIfAvailable(() -> User.createUser());
+
+        System.out.println("当前 User 对象" + user);
+    }
+
     @Bean
+    @Primary
     public String helloWorld() { // 方法名就是 bean名称 "helloWorld"
         return "hello, world";
+    }
+
+    @Bean
+    public String message() {
+        return "message";
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
