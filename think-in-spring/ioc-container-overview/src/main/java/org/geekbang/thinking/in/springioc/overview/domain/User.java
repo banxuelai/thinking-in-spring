3c28983ebcabcd5f603850e08e95c8e32e53cba8
banxuelai001 <banxuelai001@ke.com>
2022-03-31 11:14:18 +0800

    基础类型注入

--- think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/domain/User.java
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/domain/User.java
@@ -1,5 +1,9 @@
 package org.geekbang.thinking.in.springioc.overview.domain;
 
+import org.geekbang.thinking.in.springioc.overview.enums.City;
+
+import javax.annotation.Resource;
+
 /**
  * @author banxuelai001
  * @date 2020/8/21
@@ -8,6 +12,9 @@ public class User {
 
     private Long id;
     private String name;
+    private City city;
+
+    private Resource configFileLocation;
 
     public Long getId() {
         return id;
@@ -25,11 +32,29 @@ public class User {
         this.name = name;
     }
 
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Resource getConfigFileLocation() {
+        return configFileLocation;
+    }
+
+    public void setConfigFileLocation(Resource configFileLocation) {
+        this.configFileLocation = configFileLocation;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", city=" + city +
+                ", configFileLocation=" + configFileLocation +
                 '}';
     }
 
---
+++ think-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/springioc/overview/enums/City.java
@@ -0,0 +1,12 @@
+package org.geekbang.thinking.in.springioc.overview.enums;
+
+/**
+ * 城市枚举
+ * @author bxl
+ * @date 2022/3/31
+ */
+public enum City {
+    BEIJING,
+    HANGZHOU,
+    SHANGHAI
+}
